35e5877aa27a46c10e8a8ea3f78c744710603704
gyoritamas <gyoritamasnoe@gmail.com>
2022-04-08 23:59:31 +0200

    Implement subjectStudents view

---
+++ src/main/java/org/vasvari/gradebook/controllers/subjects/SubjectStudentsController.java
@@ -0,0 +1,111 @@
+package org.vasvari.gradebook.controllers.subjects;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Component;
+import org.vasvari.gradebook.dto.StudentDto;
+import org.vasvari.gradebook.service.StudentService;
+import org.vasvari.gradebook.service.SubjectService;
+import org.vasvari.gradebook.viewmodel.SubjectViewModel;
+
+import java.net.URL;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ResourceBundle;
+
+@FxmlView("view/fxml/subjects/subjectStudents.fxml")
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class SubjectStudentsController implements Initializable {
+
+    private final StudentService studentService;
+    private final SubjectService subjectService;
+
+    private Long selectedSubjectId;
+
+    @FXML
+    public GridPane subjectStudentsTab;
+    @FXML
+    public Label subjectName;
+    @FXML
+    public Label subjectTeacher;
+    @FXML
+    public ListView<StudentDto> studentsListView;
+    @FXML
+    public Button removeStudentButton;
+    @FXML
+    public ComboBox<StudentDto> studentComboBox;
+    @FXML
+    public Button addStudentButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        log.info("initialize SubjectStudentsController");
+        initializeStudentsListView();
+        initializeStudentComboBox();
+    }
+
+    private void initializeStudentsListView() {
+        log.info("initialize studentsListView");
+        if (selectedSubjectId == null) return;
+        List<StudentDto> studentsOfSubject = subjectService.findStudentsOfSubject(selectedSubjectId);
+        studentsListView.setItems(FXCollections.observableArrayList(studentsOfSubject));
+    }
+
+    private void initializeStudentComboBox() {
+        List<StudentDto> students = studentService.findAllStudents();
+        if (selectedSubjectId != null) {
+            List<StudentDto> studentsOfSubject = subjectService.findStudentsOfSubject(selectedSubjectId);
+            students.removeAll(studentsOfSubject);
+            students.sort(Comparator.comparing(StudentDto::getName));
+        }
+        studentComboBox.setItems(FXCollections.observableArrayList(students));
+    }
+
+    public void emptyEditForm() {
+        selectedSubjectId = null;
+        subjectName.setText(null);
+        subjectTeacher.setText(null);
+        studentsListView.getItems().clear();
+        studentComboBox.setValue(null);
+        subjectStudentsTab.setDisable(true);
+    }
+
+    public void populateEditForm(SubjectViewModel selectedSubject) {
+        subjectStudentsTab.setDisable(false);
+        selectedSubjectId = selectedSubject.getId();
+        subjectName.setText(selectedSubject.getName());
+        subjectTeacher.setText(selectedSubject.getTeacher().getName());
+        initializeStudentComboBox();
+        initializeStudentsListView();
+    }
+
+    @FXML
+    public void removeStudent() {
+        StudentDto selectedStudent = studentsListView.getSelectionModel().getSelectedItem();
+        if (selectedStudent == null) return;
+        subjectService.removeStudentFromSubject(selectedSubjectId, selectedStudent.getId());
+        initializeStudentsListView();
+        initializeStudentComboBox();
+    }
+
+    @FXML
+    public void addStudent() {
+        StudentDto selectedStudent = studentComboBox.getSelectionModel().getSelectedItem();
+        if (selectedStudent == null) return;
+        subjectService.addStudentToSubject(selectedSubjectId, selectedStudent.getId());
+        initializeStudentsListView();
+        initializeStudentComboBox();
+    }
+
+}
