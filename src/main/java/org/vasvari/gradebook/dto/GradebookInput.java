4bbd90e8c8efbcaad61f011299e368e6865858c1
gyoritamas <gyoritamasnoe@gmail.com>
2022-04-20 19:04:56 +0200

    Remove unnecessary field annotations from DTOs

--- src/main/java/org/vasvari/gradebook/dto/GradebookInput.java
+++ src/main/java/org/vasvari/gradebook/dto/GradebookInput.java
@@ -12,18 +12,9 @@ import javax.validation.constraints.NotNull;
 @Setter
 @Builder
 public class GradebookInput {
-    @NotNull(message = "Student ID cannot be empty")
     private Long studentId;
-
-    @NotNull(message = "Subject ID cannot be empty")
     private Long subjectId;
-
-    @NotNull(message = "Assignment ID cannot be empty")
     private Long assignmentId;
-
-    @NotNull(message = "Grade field cannot be empty")
-    @Min(value = 1, message = "Grade value must be between 1-5")
-    @Max(value = 5, message = "Grade value must be between 1-5")
     private Integer grade;
 
 }
--- src/main/java/org/vasvari/gradebook/dto/dataTypes/UsernameInput.java
+++ src/main/java/org/vasvari/gradebook/dto/dataTypes/UsernameInput.java
@@ -13,9 +13,5 @@ import javax.validation.constraints.Size;
 @Getter
 @Setter
 public class UsernameInput {
-
-    @Size(min = 4, max = 20, message = "a méretnek 4 és 20 közötti értéknek kell lennie")
-    @Pattern(regexp = "^[a-zA-Z]([0-9a-zA-Z]){3,20}",
-            message = "a felhasználónév csak betűket és számokat tartalmazhat és betűvel kell kezdődnie")
     private String username;
 }
